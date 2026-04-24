ad43871e838c85701aee48d61f0a570234b2df56
ub-victor <victoireushindi371@gmail.com>
2026-04-25 00:12:04 +0200

    Intial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 stricky top-0 z-20 backdrop-blur-xl front-medium bg-white/50 dark:bg-gay-900/70' >
+
+        </div>
+        
+    )
+}
+
+export default Navbar
