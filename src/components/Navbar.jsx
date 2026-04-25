8847cd919efc8b758b82d3cc152c25eaddbd53ba
ub-victor <victoireushindi371@gmail.com>
2026-04-25 11:52:05 +0200

    Intial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ function Navbar({theme, setTheme}) {
         <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 stricky top-0 z-20 backdrop-blur-xl front-medium bg-white/50 dark:bg-gay-900/70' >
             <img src={theme === "dark" ? assets.logo_dark : assets.logo} className='w-32 sm:w-40' alt='logo'/>
             <div>
-                <a href="#"><Home/a>
+                <a href="#" className='sm:hover:border-b'>Home</a>
+                <a href="#services" className='sm:hover:border-b'>Home</a>
             </div>
         </div>
         
