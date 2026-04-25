b6b3c9199a6ed750b5c6076863d4f0008bced42a
ub-victor <victoireushindi371@gmail.com>
2026-04-26 00:04:47 +0200

    Intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { useState } from 'react'
 
 function App() {
+    const [theme, setTheme] = useState('light')
     return (
         <div className='dark:bg-black relative'>
             <Navbar/>
