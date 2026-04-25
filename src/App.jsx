a12769ab1029a0fcb1928793b012462635432512
ub-victor <victoireushindi371@gmail.com>
2026-04-25 11:45:09 +0200

    Intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 
 function App() {
     return (
-        <div className='dark:bg'>
+        <div className='dark:bg-black relative'>
             <Navbar/>
         </div>
     )
