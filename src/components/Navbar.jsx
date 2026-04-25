faf94a98bd09254b84bff03274b614a986651e01
ub-victor <victoireushindi371@gmail.com>
2026-04-26 00:11:22 +0200

    Intial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar({theme, setTheme}) {
     return (
-        <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 stricky top-0 z-20 backdrop-blur-xl front-medium bg-white/50 dark:bg-gay-900/70' >
+        <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70' >
             <img src={theme === "dark" ? assets.logo_dark : assets.logo} className='w-32 sm:w-40' alt='logo'/>
             <div>
                 <a href="#" className='sm:hover:border-b'>Home</a>
