b72cd0bdbe6d890abbf83efed34839b2b5f87e22
ub-victor <victoireushindi371@gmail.com>
2026-04-26 00:07:05 +0200

    Intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     const [theme, setTheme] = useState('light')
     return (
         <div className='dark:bg-black relative'>
-            <Navbar theme={theme} setTheme={setTheme}/>
+            < Navbar theme={theme} setTheme={setTheme}/>
         </div>
     )
 }
