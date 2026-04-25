daba12bb28f98aad6f16443e0656c6009e44daea
ub-victor <victoireushindi371@gmail.com>
2026-04-25 11:35:56 +0200

    Intial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Navbar() {
+function Navbar({theme, setTheme}) {
     return (
         <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 stricky top-0 z-20 backdrop-blur-xl front-medium bg-white/50 dark:bg-gay-900/70' >
-
+            <img src={theme === "dark" ? ass} alt="" />
         </div>
         
     )
